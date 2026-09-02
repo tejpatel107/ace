5539d7d715993987fdd1077deca0fdb90a01fd56
tejpatel107 <rudra@acedataanalytics.com>
2026-09-02 12:11:14 +0530

    migrated files from js to ts

--- websocket/server/server.ts
+++ websocket/server/server.ts
@@ -1,15 +1,14 @@
 
 import express from "express";
-import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { WebSocketServer, WebSocket } from "ws";
 
 const app = express();
-const port = 8080;
+const port : number = 8080;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename : string = fileURLToPath(import.meta.url);
+const __dirname : string = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../client/uncoloured-grid")));
 
@@ -17,7 +16,7 @@ const server = app.listen(port, () => {
     console.log(`server is listening on ${port}...`);
 });
 
-const wss = new WebSocketServer({ server });
+const wss : WebSocketServer = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
     ws.on("message", (data) => {
